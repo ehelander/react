f2e63a01d32cc11ad22a4c4ce328bd73d61be3fd
EHelander <eric.helander@brightpeakfinancial.com>
2019-04-16 09:24:05 -0500

    Updates

---
+++ precache-manifest.5a4589a1e8a6956caf92f1bd4237a45b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5eb5c73fec294ffe0c8d",
+    "url": "/react/react-tutorial/static/js/runtime~main.e0afa707.js"
+  },
+  {
+    "revision": "bb02207377be6ab0ef2d",
+    "url": "/react/react-tutorial/static/js/main.790c1690.chunk.js"
+  },
+  {
+    "revision": "387b8d297f429e823a32",
+    "url": "/react/react-tutorial/static/js/2.17750485.chunk.js"
+  },
+  {
+    "revision": "bb02207377be6ab0ef2d",
+    "url": "/react/react-tutorial/static/css/main.449b9452.chunk.css"
+  },
+  {
+    "revision": "3d551c9a4dfec394128d92a642bd1869",
+    "url": "/react/react-tutorial/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial/precache-manifest.1f59da4400e1d5fc3db3597767337e19.js"
+  "/react/react-tutorial/precache-manifest.5a4589a1e8a6956caf92f1bd4237a45b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-tutorial/index.html", {
+workbox.routing.registerNavigationRoute("/react/react-tutorial/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
